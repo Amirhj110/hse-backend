75a90aed2ce790465f492a96e5aac379e6f53353
Amir <aamerhj110@gmail.com>
2026-03-07 23:27:24 +0500

    fix database

--- config/settings/base.py
+++ config/settings/base.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'confiq.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -76,17 +76,18 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'confiq.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        'postgresql://postgres:fvIqZdHbPmUDIWVaAxGvoHjOBDNxKoSd@maglev.proxy.rlwy.net:37542/railway',
+        conn_max_age=600
+    )
 }
 
 
