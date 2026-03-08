b71a89f4d6f1b62a800337ccd55efde1c631b30e
Amir <aamerhj110@gmail.com>
2026-03-08 13:42:12 +0500

    deploy fix continues

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,9 +4,9 @@ import dj_database_url
 
 DEBUG = False
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-temporary-key")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": dj_database_url.config(conn_max_age=600)
